f173cafc3ffe266c0467722d614498cee36edf3e
Chunyi Lyu <chunyineko@gmail.com>
2025-09-29 16:29:55 +0100

    chore: refactor kind name -> promise name
    - build group version resource from promise

--- cmd/platform_get_resources.go
+++ cmd/platform_get_resources.go
@@ -8,15 +8,20 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/syntasso/kratix/api/v1alpha1"
 	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	kratixv1alpha1 "github.com/syntasso/kratix/api/v1alpha1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/discovery/cached/memory"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/restmapper"
 )
 
@@ -29,7 +34,7 @@ const (
 	resourceNamespaceLabel = "kratix.io/component-of-resource-namespace"
 )
 
-// kratix platform get resources <promise-kind>
+// kratix platform get resources <promise-name>
 // e.g. `./bin/kratix platform get resources paved-path-demo`
 var platformGetResourcesCmd = &cobra.Command{
 	Use:   "resources PROMISE-NAME",
@@ -45,33 +50,35 @@ func init() {
 	platformGetCmd.AddCommand(platformGetResourcesCmd)
 }
 
-func clientsFromFlags(cf *genericclioptions.ConfigFlags) (dynamic.Interface, discovery.DiscoveryInterface, *apiextensionsclient.Clientset, meta.RESTMapper, error) {
+func clientsFromFlags(cf *genericclioptions.ConfigFlags) (dynamic.Interface, client.Client, *apiextensionsclient.Clientset, meta.RESTMapper, error) {
 	cfg, err := cf.ToRESTConfig()
 	if err != nil {
-		return nil, nil, nil, nil, fmt.Errorf("load REST config: %w", err)
+		return nil, nil, nil, nil, fmt.Errorf("error generating loading REST config: %w", err)
 	}
 
-	k8sClient, err := kubernetes.NewForConfig(cfg)
+	utilruntime.Must(kratixv1alpha1.AddToScheme(scheme.Scheme))
+	k8sClient, err := client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	if err != nil {
-		return nil, nil, nil, nil, fmt.Errorf("error generating client: %w", err)
+		return nil, nil, nil, nil, fmt.Errorf("error generating controller-runtime client: %w", err)
 	}
 
 	dynamicClient, err := dynamic.NewForConfig(cfg)
 	if err != nil {
-		return nil, nil, nil, nil, fmt.Errorf("dynamic client: %w", err)
+		return nil, nil, nil, nil, fmt.Errorf("error generating dynamic client: %w", err)
 	}
 
 	crdClient, err := apiextensionsclient.NewForConfig(cfg)
 	if err != nil {
-		return nil, nil, nil, nil, fmt.Errorf("create CRD client: %w", err)
+		return nil, nil, nil, nil, fmt.Errorf("error generating CRD client: %w", err)
+	}
+
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(cfg)
+	if err != nil {
+		return nil, nil, nil, nil, fmt.Errorf("error generating discovery client: %w", err)
 	}
 
 	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(discoveryClient))
-	return dynamicClient, discoveryClient, crdClient, mapper, nil
-}
-
-func gvrFor(mapper meta.RESTMapper, resource string) (schema.GroupVersionResource, error) {
-	return mapper.ResourceFor(schema.GroupVersionResource{Resource: resource})
+	return dynamicClient, k8sClient, crdClient, mapper, nil
 }
 
 func listAllKratixGVRs(crdClient apiextensionsclient.Interface) ([]schema.GroupVersionResource, error) {
@@ -107,20 +114,17 @@ func listAllKratixGVRs(crdClient apiextensionsclient.Interface) ([]schema.GroupV
 
 func renderTree(promiseName string) error {
 	ctx := context.Background()
-	dynamicClient, _, crdClient, mapper, err := clientsFromFlags(configFlags)
+	dynamicClient, k8sClient, crdClient, mapper, err := clientsFromFlags(configFlags)
 	if err != nil {
 		return err
 	}
 
-	promise := 
-
-
-	gvr, err := gvrFor(mapper, promiseName)
-	if err != nil {
-		return fmt.Errorf("error generating GroupVersionResource for %q, is the specified kind correct? %s", err)
+	gvr, gvrErr := gvrForPromise(ctx, k8sClient, mapper, promiseName)
+	if gvrErr != nil {
+		return gvrErr
 	}
 
-	promiseRequests, err := dynamicClient.Resource(gvr).Namespace(v1.NamespaceAll).List(ctx, v1.ListOptions{})
+	promiseRequests, err := dynamicClient.Resource(*gvr).Namespace(v1.NamespaceAll).List(ctx, v1.ListOptions{})
 	if err != nil {
 		return fmt.Errorf("list %q : %w", kind, err)
 	}
@@ -177,4 +181,38 @@ func renderTree(promiseName string) error {
 
 	fmt.Print(b.String())
 	return nil
+}
+
+func gvrForPromise(ctx context.Context, k8sClient client.Client, mapper meta.RESTMapper, promiseName string) (*schema.GroupVersionResource, error) {
+	promise := &v1alpha1.Promise{}
+	err := k8sClient.Get(ctx, types.NamespacedName{Name: promiseName}, promise)
+
+	if errors.IsNotFound(err) {
+		return nil, fmt.Errorf("promise: %s not found", promiseName)
+	}
+	if client.IgnoreNotFound(err) != nil {
+		return nil, fmt.Errorf("error getting promise: %s with error %q", promiseName, err)
+	}
+
+	if !promise.ContainsAPI() {
+		return nil, fmt.Errorf("promise: %s contains no API", promiseName)
+	}
+
+	gvk, _, apiErr := promise.GetAPI()
+	if apiErr != nil {
+		return nil, fmt.Errorf("error generating GVK from promise: %s with error %q", promiseName, apiErr)
+	}
+
+	gvr, err := buildGVR(mapper, gvk.Group, gvk.Version, gvk.Kind)
+	if err != nil {
+		return nil, fmt.Errorf("error generating GroupVersionResource: %q", err)
+	}
+	return &gvr, nil
+}
+
+func buildGVR(mapper meta.RESTMapper, group, version, resource string) (schema.GroupVersionResource, error) {
+	return mapper.ResourceFor(schema.GroupVersionResource{
+		Group:    group,
+		Version:  version,
+		Resource: resource})
 }
