f45bbcdbf2d4430ce3cb5bc3503abf93799a8bf3
sapphire <sapphire@syntasso.io>
2025-10-06 11:42:41 +0100

    DEBUG: log path

--- cmd/container_utils/build.go
+++ cmd/container_utils/build.go
@@ -51,6 +51,8 @@ func ForkRunCommand(opts *BuildContainerOptions, containerImage, inputVolume, ou
 
 	builder := exec.Command(opts.Engine, buildArgs...)
 	fmt.Println(buildArgs)
+	fmt.Println(builder.Path)
+
 	builder.Stdout = os.Stdout
 	builder.Stderr = os.Stderr
 	if err := builder.Run(); err != nil {
