e739ae394caa2c8789a9847f5bdece9b514146cb
Sapphire Mason-Brown <saphmb@gmail.com>
2025-10-08 10:16:18 +0100

    Update cmd/utils/utils.go
    Co-authored-by: Chunyi Lyu <chunyineko@gmail.com>

--- cmd/utils/utils.go
+++ cmd/utils/utils.go
@@ -3,8 +3,6 @@ package utils
 import "os"
 
 func FileExists(filePath string) bool {
-	if _, err := os.Stat(filePath); err != nil {
-		return false
-	}
-	return true
+	_, err := os.Stat(filePath)
+	return err == nil || !os.IsNotExist(err)
 }
